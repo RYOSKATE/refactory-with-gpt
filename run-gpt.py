9abafa1b448ef494d126a201071546547c99d73d
RYOSKATE <ryoskate@gmail.com>
2023-06-19 19:40:06 +0900

    メモ追加

--- run-gpt.py
+++ run-gpt.py
@@ -4,57 +4,55 @@ import os
 openai.organization = os.getenv("OPENAI_ORGANIZATION")
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+
 def Ask_ChatGPT(message):
-    
+
     # 応答設定
     completion = openai.ChatCompletion.create(
-                 model    = "gpt-3.5-turbo",     # モデルを選択
-                 messages = [
-                    {"role":"user","content":message,}],
-    
-                 max_tokens  = 1024,             # 生成する文章の最大単語数
-                 n           = 1,                # いくつの返答を生成するか
-                 stop        = None,             # 指定した単語が出現した場合、文章生成を打ち切る
-                 temperature = 0.5,              # 出力する単語のランダム性（0から2の範囲） 0であれば毎回返答内容固定
+        model="gpt-3.5-turbo",     # モデルを選択
+        messages=[{"role": "user", "content": message, }],
+        max_tokens=1024,             # 生成する文章の最大単語数
+        n=1,                # いくつの返答を生成するか
+        stop=None,             # 指定した単語が出現した場合、文章生成を打ち切る
+        temperature=0.5,              # 出力する単語のランダム性（0から2の範囲） 0であれば毎回返答内容固定
     )
-    
+
     # 応答
     response = completion.choices[0].message.content
-    
+
     # 応答内容出力
     return response
 
 
 # 質問内容
 message = """
-以下の仕様に従って答えてください。
+Please answer according to the following specifications.
 
-# 返答のフォーマット
-{"code":"入力されたPythonプログラムを修正したソースコード"}
+# Reply format
+{"code": "Source code of the modified Python program entered"}
 
-# 命令
-あなたはPythonプログラムの講師です。
-生徒が提出したソースコードの誤りを、以下のルールを必ず守って修正し、回答してください。
+# Instruction
+You are an instructor of Python programs.
+Please correct the errors in the source code submitted by your students and answer the questions by following the rules below without fail.
 
-## ルール
-- 問題文は #問題文 に続いて記載されている。
-- 提出コードは #提出コード に続くコードブロックに記載されている。
-- 問題文の回答になるように、提出コードを正しく修正すること
-- コメント文を追加・削除しないこと
-- 空白文字やを追加・削除しないこと
-- 改行文字を追加、削除しないこと
-- 変数名や関数名を変更しないこと
-- 修正は最小限にすること
-- 返答はJSON形式にすること。
-- 問題点を修正したプログラムは出力するJSONの`code`フィールドにセットすること
+## Rules
+- The problem statement follows the # problem statement.
+- Submitted code is listed in the code block following the # submitted code.
+- Correctly modify the submission code to answer the problem statement.
+- Do not add or delete comment text.
+- Do not add or delete whitespace or
+- Do not add or delete line break characters
+- Do not change variable or function names
+- Modifications should be kept to a minimum.
+- The reply should be in JSON format.
+- The program that fixes the problem should be set in the `code` field of the output JSON
 
-仕様は以上です。
+That's all for the specification.
 
-# 問題文
-配列seqを先頭から探索し、最初に見つかるx以下の値の要素のインデックスを返すプログラムを作成せよ
+# Problem statement
+Create a program that searches an array seq from the beginning and returns the index of the first element of value less than or equal to x found.
 
-
-# 提出コード
+# Submission Code
 ```python
 def search(x, seq):
     if seq == [] or seq == ():
@@ -69,13 +67,15 @@ def search(x, seq):
                 return i
             elif seq[i] <= x and seq[i+1] > x:
                 return i+1
+```
 
-
+```
+一番近い
 ```
 """
 
 # ChatGPT起動
-res = Ask_ChatGPT( message)
+res = Ask_ChatGPT(message)
 
 # 出力
 print(res)
