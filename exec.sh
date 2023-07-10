5cae6d30eb39e6f642d4a120865bc87bd79977b6
RYOSKATE <ryoskate@gmail.com>
2023-07-10 20:12:01 +0900

    まとめて実行用.sh更新

--- exec.sh
+++ exec.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 python3 run.py -d ./data -q question_1 -s $1 -o -m -b   
-python3 run.py -d ./data -q question_1 -s $1 -o -m -b -g
+python3 run.py -d ./data -q question_1 -s $1 -o -m -b -g both
 python3 run.py -d ./data -q question_2 -s $1 -o -m -b   
-python3 run.py -d ./data -q question_2 -s $1 -o -m -b -g
+python3 run.py -d ./data -q question_2 -s $1 -o -m -b -g both
 python3 run.py -d ./data -q question_3 -s $1 -o -m -b   
-python3 run.py -d ./data -q question_3 -s $1 -o -m -b -g
+python3 run.py -d ./data -q question_3 -s $1 -o -m -b -g both
 python3 run.py -d ./data -q question_4 -s $1 -o -m -b   
-python3 run.py -d ./data -q question_4 -s $1 -o -m -b -g
+python3 run.py -d ./data -q question_4 -s $1 -o -m -b -g both
 python3 run.py -d ./data -q question_5 -s $1 -o -m -b   
-python3 run.py -d ./data -q question_5 -s $1 -o -m -b -g
+python3 run.py -d ./data -q question_5 -s $1 -o -m -b -g both
