8dceccc3d54fd567efd7190ea08a5eb87d5e9d25
RYOSKATE <ryoskate@gmail.com>
2023-07-14 08:58:47 +0900

    結果ディレクトリを分割

--- exec.sh
+++ exec.sh
@@ -10,8 +10,9 @@ python3 run.py -d ./data -q question_4 -s $1 -o -m -b -g both
 python3 run.py -d ./data -q question_5 -s $1 -o -m -b
 python3 run.py -d ./data -q question_5 -s $1 -o -m -b -g both
 
-rm -rf results/20230703
-zip -r results-$1.zip results
+rm -rf results/.gitkeep
+mv results/ results-$1/
+zip -r results-$1.zip results-$1
 
 curl -F file=@./results-$1.zip  -F "initial_comment=$1終了"  -F channels=#research -F token=$SLACK_TOKEN https://slack.com/api/files.upload
 
