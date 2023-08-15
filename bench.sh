76fe8747d43d604b542aff61248776dac093ec90
Sakamoto, Kazunori <exkazuu@gmail.com>
2023-08-15 21:49:06 +0900

    .

---
+++ bench.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+python3 run.py -d ./data -q question_1 -s $1 -o -m -b -g both
+python3 run.py -d ./data -q question_2 -s $1 -o -m -b -g both
+python3 run.py -d ./data -q question_3 -s $1 -o -m -b -g both
+python3 run.py -d ./data -q question_4 -s $1 -o -m -b -g both
+python3 run.py -d ./data -q question_5 -s $1 -o -m -b -g both
