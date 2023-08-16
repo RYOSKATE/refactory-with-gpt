93bcd07ed9007e69d46f5bc876881659ab162061
Sakamoto, Kazunori <exkazuu@gmail.com>
2023-08-16 20:20:29 +0900

    .

--- benchmark.py
+++ benchmark.py
@@ -16,7 +16,7 @@ def main():
 
     # json_file_names = ['cc1ec49af3dec285599a05918d26cf0b847c657f8a2d85bf0b5cce69c1e43578.json']
     # json_file_names = ['2558a8555f9b8bf9a18a31da5b0160f87d4eccfb9d124446de558445fdeeec37.json', '6b15490da95ed587034925cf2f2a869f4774df2adb5596787f9e4a2f58b7611c.json', 'cc1ec49af3dec285599a05918d26cf0b847c657f8a2d85bf0b5cce69c1e43578.json', '4ee6cf842a7d1531bcfd39697901e7852c3e42439d8c583e2b53864add42f8ca.json']
-    # json_file_names = ['78ea585010b23b20e51601e486946cd29fe37362b597bb8a2a5fa9edfb7a89f0.json']
+    json_file_names = ['78ea585010b23b20e51601e486946cd29fe37362b597bb8a2a5fa9edfb7a89f0.json', 'cc1ec49af3dec285599a05918d26cf0b847c657f8a2d85bf0b5cce69c1e43578.json']
 
     win = 0
     draw = 0
@@ -36,14 +36,15 @@ def main():
 
         print('# Repaired Code by GPT-3.5-Turbo and Old Prompt')
         print(data['gpt_rep_code'])
-        old_patch_size = zss_multi_func_code_distance(data['bug_code'], data['gpt_rep_code'])
+        regularized_bug_code = regularize(data['bug_code'])
+        old_patch_size = zss_multi_func_code_distance(regularized_bug_code, data['gpt_rep_code'])
 
         gpt_model = data['gpt_model']
         # gpt_model = 'gpt-4'
         raw_rep_code: str = repair_code_by_gpt_with_retry(data['bug_code'], data['description'], data['sample_correct_code_blocks'], gpt_model) or ''
 
         rep_code = regularize(raw_rep_code)
-        patch_size = zss_multi_func_code_distance(data['bug_code'], rep_code)
+        patch_size = zss_multi_func_code_distance(regularized_bug_code, rep_code)
 
         print('# Repaired Code by GPT-3.5-Turbo and New Prompt')
         print(rep_code)
