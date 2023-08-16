033bd8b459dc8778cea0372e0a4217947bc0a12b
Sakamoto, Kazunori <exkazuu@gmail.com>
2023-08-16 14:37:29 +0900

    .

--- basic_framework/repair_with_gpt.py
+++ basic_framework/repair_with_gpt.py
@@ -29,6 +29,10 @@ def repair_code_by_gpt_with_retry(bug_code: str, description: str, sample_correc
             retry_count += 1
             continue
 
+        print('----------')
+        print(generated_text)
+        print('----------')
+
         code = get_code_blocks(generated_text)
 
         # コード部分が空の場合
@@ -72,39 +76,56 @@ def repair_code_by_gpt_with_retry(bug_code: str, description: str, sample_correc
 
 def _repair_code_by_gpt(bug_code: str, description: str, sample_correct_code_blocks: list[str], gpt_model="gpt-3.5-turbo", extra_messages: list[str] = []) -> tuple[str, bool]:
 
-    order = f"""
-    description: "${description}"
+    prompt = f"""
+Act as an expert in Python programming and create a patch to fix the Python program code for the problem following the rules.
 
-    ```python
-    {bug_code}
-    ```
+# Rules
+- The patch should be as close as possible to the original code.
+- Keep the patch size as small as possible.
+- Keep the original program structure as much as possible.
+- Keep the original order of the statements as much as possible.
+- Keep `pass`, `break` and `continue` statements as much as possible.
+- Keep the original function and vairable names, and ignore the names in reference code.
+- Keep the original parentheses as much as possible even though they are redundant.
+- Keep the original statements as much as possible even though they are redundant.
+- Keep the original conditional branches as much as possible even though they are redundant.
 
-    Please modify the above Python program code to work correctly as specified in the description.
-    Please observe the following rules when outputting code.
+# Problem description
+{description}
 
-    - Code is enclosed in Markdown code blocks.
-    - Formatted to be executable with Python's exec function.
-    - Keep the modified code as many characters as possible as the original code.
-    - Do not add or delete comment text.
-    - Do not add or delete whitespace or.
-    - Do not add or delete line break characters.
-    - Do not change variable or function names.
-    - Please modify the code to be closer to the unmodified code than the following model solution.
-    
-    {sample_correct_code_blocks}
+# Original code to be fixed
+```python
+{bug_code}
+```
+"""
 
-    """
+    for index in range(len(sample_correct_code_blocks)):
+        prompt += f"""
+# Model solution {index + 1} (Ignore naming rules)
+{sample_correct_code_blocks[index]}
+"""
+
+        prompt += f'''
+# Output format
+"""
+# Fixed code with fewest changes
+```python
+...
+```
+"""
+'''
+
+
     completion = openai.ChatCompletion.create(
         model=gpt_model,
         messages=[
-            {"role": "system", "content": "You are an excellent Python programmer."},
-            {"role": "user", "content": order},
+            {"role": "system", "content": prompt},
             *extra_messages,
         ],
         max_tokens=1024,    # 生成する文章の最大単語数
         n=1,                # いくつの返答を生成するか
         stop=None,          # 指定した単語が出現した場合、文章生成を打ち切る
-        temperature=0.0,    # 出力する単語のランダム性（0から2の範囲） 0であれば毎回返答内容固定
+        temperature=0.7,    # 出力する単語のランダム性（0から2の範囲） 0であれば毎回返答内容固定
     )
     return completion.choices[0].message.content
 
